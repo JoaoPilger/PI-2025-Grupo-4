40d5374172d389d2c255b33f24528166d13700eb
BiancaGolfe <biancagabrielagolfe@gmail.com>
2025-07-25 13:29:54 -0300

    arrumei a responsividade do cadastro e login
    arrumei a responsividade do cadastro e login

--- PI-2025/src/App.jsx
+++ PI-2025/src/App.jsx
@@ -1,12 +1,12 @@
-//import Duvidas from './pages/duvidas/duvidas.jsx'
-import Login from './pages/cadastroelogin/login.jsx'
-import Cadastro from "./pages/cadastroelogin/cadastro.jsx"
+import Duvidas from './pages/duvidas/duvidas.jsx'
+//import Login from './pages/cadastroelogin/login.jsx'
+//import Cadastro from "./pages/cadastroelogin/cadastro.jsx"
 function App() {
   return (
     <>
-    <Cadastro />
-    <Login />
-    {/* <Duvidas /> */}
+    {/* <Cadastro /> */}
+    {/* <Login /> */}
+    <Duvidas />
     </>
   )
 }
